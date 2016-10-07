375fe030066a4a0f082631a70445d42ce0bec107
Sebastiaan Fernandez <sebastiaan.fernandez.w1944@student.hogent.be>
2016-10-07 12:27:35 +0200

    Add projects

---
+++ test/BlackJackGame.Tests/Models/BlackJackCardTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlackJackGame.Models
+{
+    public class BlackJackCardTest
+    {
+        [Fact]
+        public void TestOmDeTest()
+        {
+            Assert.Equal(2, 1+1);
+        }
+    }
+}
