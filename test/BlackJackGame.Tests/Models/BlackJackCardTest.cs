a66bf91349f1e4dc66bf0bc1e0a33bac07d401ce
Sebastiaan Fernandez <sebastiaan.fernandez.w1944@student.hogent.be>
2016-10-13 13:50:22 +0200

    Add test classes

--- test/BlackJackGame.Tests/Models/BlackJackCardTest.cs
+++ test/BlackJackGame.Tests/Models/BlackJackCardTest.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BlackJackGame.Models;
 using Xunit;
 
-namespace BlackJackGame.Models
-{
-    public class BlackJackCardTest
-    {
+
+namespace BlackJackGame.Tests.Models {
+
+    public class BlackJackCardTest {
+
         [Fact]
-        public void TestOmDeTest()
-        {
-            Assert.Equal(2, 1+1);
+        public void NewBlackJackCard_CreatedCorrectly() {
+            BlackJackCard card = new BlackJackCard(Suit.Hearts, FaceValue.Ace);
+            Assert.Equal(Suit.Hearts, card.Suit);
+            Assert.Equal(FaceValue.Ace, card.FaceValue);
+            Assert.False(card.FaceUp);
+        }
+
+        [Fact]
+        public void TurnCard_CardFaceDown_TurnsCardToFaceUp() {
+            BlackJackCard card = new BlackJackCard(Suit.Hearts, FaceValue.Ace);
+            card.TurnCard();
+            Assert.True(card.FaceUp);
+        }
+
+        [Fact(Skip = "Not yet implemented")]
+        public void TurnCard_CardFaceUp_TurnsCardToFaceDown() {
+            BlackJackCard card = new BlackJackCard(Suit.Hearts, FaceValue.Ace);
+            card.TurnCard();
+            card.TurnCard();
+        }
+
+        [Fact(Skip = "Not yet implemented")]
+        public void Value_CardFaceUp_GivesCorrectValue() {
+            BlackJackCard card = new BlackJackCard(Suit.Hearts, FaceValue.King);
+            card.TurnCard();
+        }
+
+        [Fact(Skip = "Not yet implemented")]
+        public void Value_CardFaceDown_IsZero() {
+           BlackJackCard card = new BlackJackCard(Suit.Hearts, FaceValue.Ace);
         }
     }
 }
